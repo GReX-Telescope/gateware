515b6d073bcd561ecad5b1f5e39de54669d7722a
Kiran Shila <me@kiranshila.com>
2022-06-16 10:37:45 -0700

    More toolflow setup

--- compile_design.sh
+++ compile_design.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
+venv=$(poetry env info -p)
+
 # Export all the variables casper expects
 export XILINX_PATH=/opt/Xilinx/Vivado/2019.1
-export MATLAB_PATH=/opt/MATLAB/R2018a
+export MATLAB_PATH=/opt/MATLAB/R2019a
 export PLATFORM=lin64
 export JASPER_BACKEND=vivado
 export CASPER_PYTHON_VENV_ON_START="$venv"
 export LD_PRELOAD=${LD_PRELOAD}:"/usr/lib/libfreetype.so"
 export LD_PRELOAD=${LD_PRELOAD}:"/usr/lib/libstdc++.so"
-export MATLAB_JAVA=/usr/lib/jvm/java-8-adoptopenjdk/jre
+export LD_PRELOAD=${LD_PRELOAD}:"/lib/libgio-2.0.so"
+export LD_PRELOAD=${LD_PRELOAD}:"/lib/libglib-2.0.so"
+export LD_PRELOAD=${LD_PRELOAD}:"/lib/libcrypto.so"
 
-. mlib_devel/startsg
+. startsg
 
 poetry run python mlib_devel/jasper_library/exec_flow.py \
-    -m "$(pwd)/test_snap.slx"                            \
+    -m "$(pwd)/grex_gateware.slx"                        \
     --perfile                                            \
     --frontend                                           \
     --middleware                                         \
--- start_toolflow.sh
+++ start_toolflow.sh
@@ -6,13 +6,12 @@ venv=$(poetry env info -p)
 
 # Export all the variables casper expects
 export XILINX_PATH=/opt/Xilinx/Vivado/2019.1
-export MATLAB_PATH=/opt/MATLAB/R2018a
+export MATLAB_PATH=/opt/MATLAB/R2019a
 export PLATFORM=lin64
 export JASPER_BACKEND=vivado
 export CASPER_PYTHON_VENV_ON_START="$venv"
 export LD_PRELOAD=${LD_PRELOAD}:"/usr/lib/libfreetype.so"
 export LD_PRELOAD=${LD_PRELOAD}:"/usr/lib/libstdc++.so"
-export MATLAB_JAVA=/usr/lib/jvm/java-8-adoptopenjdk/jre
 
 # Run the toolflow
-./mlib_devel/startsg
+./startsg

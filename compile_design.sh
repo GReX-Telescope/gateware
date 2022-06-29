75a43eab862274ed807a6e562c4a660785202bba
Kiran Shila <me@kiranshila.com>
2022-06-29 12:38:08 -0700

    First end to end design

--- compile_design.sh
+++ compile_design.sh
@@ -4,15 +4,15 @@ venv=$(poetry env info -p)
 
 # Export all the variables casper expects
 export XILINX_PATH=/opt/Xilinx/Vivado/2019.1
-export MATLAB_PATH=/opt/MATLAB/R2019a
+export MATLAB_PATH=/opt/MATLAB/R2018a
 export PLATFORM=lin64
 export JASPER_BACKEND=vivado
 export CASPER_PYTHON_VENV_ON_START="$venv"
-export LD_PRELOAD=${LD_PRELOAD}:"/usr/lib/libfreetype.so"
-export LD_PRELOAD=${LD_PRELOAD}:"/usr/lib/libstdc++.so"
-export LD_PRELOAD=${LD_PRELOAD}:"/lib/libgio-2.0.so"
-export LD_PRELOAD=${LD_PRELOAD}:"/lib/libglib-2.0.so"
-export LD_PRELOAD=${LD_PRELOAD}:"/lib/libcrypto.so"
+#export LD_PRELOAD=${LD_PRELOAD}:"/usr/lib/libfreetype.so"
+#export LD_PRELOAD=${LD_PRELOAD}:"/usr/lib/libstdc++.so"
+#export LD_PRELOAD=${LD_PRELOAD}:"/lib/libgio-2.0.so"
+#export LD_PRELOAD=${LD_PRELOAD}:"/lib/libglib-2.0.so"
+#export LD_PRELOAD=${LD_PRELOAD}:"/lib/libcrypto.so"
 
 . startsg
 
--- start_toolflow.sh
+++ start_toolflow.sh
@@ -6,12 +6,10 @@ venv=$(poetry env info -p)
 
 # Export all the variables casper expects
 export XILINX_PATH=/opt/Xilinx/Vivado/2019.1
-export MATLAB_PATH=/opt/MATLAB/R2019a
+export MATLAB_PATH=/opt/MATLAB/R2018a
 export PLATFORM=lin64
 export JASPER_BACKEND=vivado
 export CASPER_PYTHON_VENV_ON_START="$venv"
-export LD_PRELOAD=${LD_PRELOAD}:"/usr/lib/libfreetype.so"
-export LD_PRELOAD=${LD_PRELOAD}:"/usr/lib/libstdc++.so"
 
 # Run the toolflow
 ./startsg
